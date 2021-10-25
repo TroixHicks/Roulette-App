8bc03fa6a508847f07c01b5ea69ad8f6d8e865b4
Troi Hicks <88952205+TroixHicks@users.noreply.github.com>
2021-10-24 21:07:19 -0400

    A Roulette App

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : "mongodb+srv://troi:Troiana123@cluster0.lyfc9.mongodb.net/roulette?retryWrites=true&w=majority", 
+    'dbName': 'roulette'
+};
